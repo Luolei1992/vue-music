86953c6285a919226a9aa18468748336949ae670
Luolei1992 <ll251537@foxmail.com>
2018-03-29 00:16:46 +0800

    架构搭建

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        <nav-header></nav-header>
+    </div>
+</template>
+
+<script>
+    import NavHeader from './components/m-header/m-header'
+    export default {
+        data(){
+
+        },
+        components: {
+            NavHeader
+        }
+    }
+</script>
+
+<style lang="stylus" scoped rel="stylesheet/stylus">
+
+</style>
