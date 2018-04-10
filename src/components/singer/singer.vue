e92177e6980c50e4de7f84b836747ca3c903af5c
Luolei1992 <ll251537@foxmail.com>
2018-04-10 23:26:02 +0800

    使用jsonp获取歌手列表

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,20 +1,41 @@
 <template>
-  <div class="singer" ref="singer">
-    singer
-  </div>
+    <div class="singer" ref="singer">
+        singer
+    </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+
+import {getSingerList} from 'api/singer.js'
+import {ERR_OK} from 'api/config.js'
+export default {
+    data(){
+        return {
+            singers:[]
+        }
+    },
+    created(){
+        this._getSingerList()
+    },
+    methods:{
+        _getSingerList(){
+            getSingerList().then((res)=>{
+                console.log(res.data.list);
+                if(res.code === ERR_OK) {
+                    this.singers = res.data.list
+                }
+            })
+        }
+    }
+}
 
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  .singer
-    position: fixed
-    top: 88px
-    bottom: 0
-    width: 100%
+.singer {
+    position: fixed;
+    top: 88px;
+    bottom: 0;
+    width: 100%;
+}
 </style>
