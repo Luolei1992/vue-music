dfa3c224f6bfabf68101052b8be8819c149c24db
Luolei1992 <ll251537@foxmail.com>
2018-04-16 00:39:43 +0800

    添加vuex 歌单列表页

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,6 @@
 <template>
     <transition name="slide">
-        <div class="singer-detail"></div>
+        <music-list :songs="songs" :title="title" :bg-image="bgImage"></music-list>
     </transition>
 </template>
 
@@ -9,6 +9,7 @@ import {mapGetters} from 'vuex'
 import {getSingerDetail} from 'api/singer'
 import { ERR_OK } from 'api/config'
 import {createSong} from 'common/js/song'
+import MusicList from 'components/music-list/music-list'
 export default {
     data(){
         return {
@@ -16,6 +17,12 @@ export default {
         }
     },
     computed:{
+        title(){
+            return this.singer.name
+        },
+        bgImage(){
+            return this.singer.avatar
+        },
         ...mapGetters([
             'singer'
         ])
@@ -48,6 +55,9 @@ export default {
             });
             return lis;
         }
+    },
+    components:{
+        MusicList
     }
 }
 </script>
