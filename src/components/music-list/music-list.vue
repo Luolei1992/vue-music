8da4103915aefa75d7c59de70d54a01bee44105d
Luolei1992 <ll251537@foxmail.com>
2018-04-16 18:48:56 +0800

    歌手详情页完善

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,10 +1,16 @@
 <template>
     <div class="music-list">
-        <div class="back">
+        <div class="back" @click="back">
             <i class="icon-back"></i>
         </div>
         <h1 class="title" v-html="title"></h1>
         <div class="bg-image" :style="bgStyle" ref="bgImage">
+            <div class="play-wrapper">
+                <div ref="playBtn" v-show="songs.length>0" class="play" @click="random">
+                    <i class="icon-play"></i>
+                    <span class="text">随机播放全部</span>
+                </div>
+            </div>
             <div class="filter" ref="filter"></div>
         </div>
         <div class="bg-layer" ref="layer">
@@ -24,7 +30,10 @@
 <script type="text/ecmascript-6">
 import Scroll from 'base/scroll/scroll'
 import SongList from 'base/song-list/song-list'
+import { prefixStyle } from 'common/js/dom'
 const topHeight = 40;
+const transform = prefixStyle('transform')
+const backdrop = prefixStyle('backdrop-filter')
 export default {
     props: {
         bgImage: {
@@ -63,15 +72,22 @@ export default {
     methods: {
         scroll(pos) {
             this.scrollY = pos.y
-        }
+        },
+        back() {
+            this.$router.back()
+        },
+        random() {
+            this.randomPlay({
+                list: this.songs
+            })
+        },
     },
     watch: {
         scrollY(newY) {
             let translateY = Math.max(this.minTranslateY, newY)
             let zIndex = 0
             let scale = 1
-            this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`
-            this.$refs.layer.style['webkit-transform'] = `translate3d(0,${translateY}px,0)`
+            this.$refs.layer.style[transform] = `translate3d(0,${translateY}px,0)`
             const percent = Math.abs(newY / this.imageHeight)
             if (newY > 0) {
                 scale = 1 + percent
@@ -84,13 +100,13 @@ export default {
                 zIndex = 10
                 this.$refs.bgImage.style.paddingTop = 0
                 this.$refs.bgImage.style.height = `${topHeight}px`
-                // this.$refs.playBtn.style.display = 'none'
+                this.$refs.playBtn.style.display = 'none'
             } else {
                 this.$refs.bgImage.style.paddingTop = '70%'
                 this.$refs.bgImage.style.height = 0
-                // this.$refs.playBtn.style.display = ''
+                this.$refs.playBtn.style.display = ''
             }
-            this.$refs.bgImage.style['transform'] = `scale(${scale})`
+            this.$refs.bgImage.style[transform] = `scale(${scale})`
             this.$refs.bgImage.style.zIndex = zIndex
         }
     },
